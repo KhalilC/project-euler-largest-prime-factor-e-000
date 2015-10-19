523036b726e0c64f695f4988f59c71ae37b2ead3
KhalilC <klcrumpler@gmail.com>
2015-10-19 18:55:13 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+require 'prime'
+
+def largest_prime_factor(number)
+  array = number.prime_division.map(&:first)
+  array.max
+end
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+
+require 'prime'
+
+class LargestPrimeFactor
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def number
+    array = input.prime_division.map(&:first)
+    array.max
+  end
+end
